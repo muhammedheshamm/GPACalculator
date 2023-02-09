7b9e6975a0a73ba337a6cbaba6b7751de74990cc
muhammed hesham <muhfs@github.com>
2023-02-09 15:07:45 +0200

    updated

--- js/index.js
+++ js/index.js
@@ -172,7 +172,7 @@ function AgradeToNumeric(grade){
         case "B" : return 3
         case "B-" : return 2.7  
         case "C+" : return 2.3 
-        case "C" : return 3  
+        case "C" : return 2  
         case "C-" : return 1.7  
         case "D+" : return 1.3  
         case "D" : return 1  
